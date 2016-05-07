427d7bf9f73a1a3100dfd323dedca4af0b6c81a7
Lucy Linder <lucy.derlin@gmail.com>
2016-05-07 10:08:13 +0200

    add a message box to modify the axis on double click

--- webapp/ajs/app.controller.js
+++ webapp/ajs/app.controller.js
@@ -9,155 +9,189 @@
  */
 (function(){
 
-    /**
-     * @ngdoc controller
-     * @name bbdata.app.MainCtrl
-     *
-     * @description
-     * Main controller
-     */
-    angular
-        .module( 'bbdata.app' )
-        .controller( 'MainCtrl', MainCtrl );
+        /**
+         * @ngdoc controller
+         * @name bbdata.app.MainCtrl
+         *
+         * @description
+         * Main controller
+         */
+        angular
+            .module( 'bbdata.app' )
+            .controller( 'MainCtrl', MainCtrl );
 
-    // --------------------------
+        // --------------------------
 
-    function MainCtrl( RestService, $scope, $filter ){
+        function MainCtrl( RestService, $scope, $filter ){
 
-        var self = this;
+            var self = this;
 
-        self.test = "it works!";
-        self.captorsHierarchy = [];
-        self.chkChanged = checkboxChanged;
-        _init();
-
-
-        self.date = {
-            from: moment().floor( 1, 'hour' ).subtract( 1, 'hour' ).toDate(),
-            to  : moment().floor( 1, 'hour' ).toDate()
-        };
-
-        self.applyDate = applyDate;
+            self.test = "it works!";
+            self.captorsHierarchy = [];
+            self.modifiedAxis = null;
+            self.chkChanged = checkboxChanged;
+            self.applyAxisChanges = applyAxisChanges;
+            _init();
 
 
-        var chart = null;
-        var defaultChartOptions = {
-            chart        : {
-                renderTo: 'graphContainer'
-            },
-            rangeSelector: {
-                enabled: false
-            },
+            self.date = {
+                from: moment().floor( 1, 'hour' ).subtract( 1, 'hour' ).toDate(),
+                to  : moment().floor( 1, 'hour' ).toDate()
+            };
 
-            legend: {enabled: true}
-        };
+            self.applyDate = applyDate;
 
 
-        /* *****************************************************************
-         * implementation
-         * ****************************************************************/
-
-        //##------------scope power
-
-        $scope.copy = angular.copy;
-        $scope.equals = angular.equals;
-
-        //##------------init
-
-        function _init(){
-            RestService.getHierarchy( function( result ){
-                self.captorsHierarchy = result;
-            }, _handleError );
-
-        }
-
-
-        //##------------available methods
-
-        function checkboxChanged( item ){
-            console.log( item );
-            if( item.selected ){
-                // ajax
-                getValues( item );
-
-            }else{
-                removeSerie( item );
-            }
-        }
-
-        function addSerie( item, values ){
-            console.log( values );
-
-            var axis = { // Secondary yAxis
-                id      : item.id + "-axis",
-                title   : {
-                    text: item.name
+            var chart = null;
+            var defaultChartOptions = {
+                chart        : {
+                    renderTo: 'graphContainer'
                 },
-                opposite: true
+                rangeSelector: {
+                    enabled: false
+                },
+
+
+                legend: {enabled: true}
             };
 
-            var serie = {
-                name : item.name,
-                id   : item.id + "-serie",
-                yAxis: item.id + "-axis",
-                data : toTrace( values )
-            };
 
-            if( !chart ){
-                var options = angular.copy( defaultChartOptions );
-                options.series = [serie];
-                options.yAxis = axis;
+            /* *****************************************************************
+             * implementation
+             * ****************************************************************/
 
-                chart = new Highcharts.StockChart( options );
-                self.chart = chart;
+            //##------------scope power
 
-            }else{
-                chart.addAxis( axis );
-                chart.addSeries( serie );
+            $scope.copy = angular.copy;
+            $scope.equals = angular.equals;
+
+            //##------------init
+
+            function _init(){
+                RestService.getHierarchy( function( result ){
+                    self.captorsHierarchy = result;
+                }, _handleError );
+
+            }
+
+
+            //##------------available methods
+
+            function checkboxChanged( item ){
+                console.log( item );
+                if( item.selected ){
+                    // ajax
+                    getValues( item );
+
+                }else{
+                    removeSerie( item );
+                }
+            }
+
+            function addSerie( item, values ){
+                console.log( values );
+
+                var axis = { // Secondary yAxis
+                    id   : item.id + "-axis",
+                    title: {
+                        text  : item.name,
+                        events: {
+                            dblclick: function(){
+                                console.log( this );
+                                console.log( this.min, this.max );
+                                if( !this.mydefaults ){
+                                    this.mydefaults = [this.min, this.max];
+                                }
+                                self.modifiedAxis = {
+                                    title: this.userOptions.title.text,
+                                    min  : this.min,
+                                    max  : this.max,
+                                    elem : this
+                                };
+
+                                $scope.$apply();
+
+                            }
+                        }
+                    }
+                };
+
+                var serie = {
+                    name : item.name,
+                    id   : item.id + "-serie",
+                    yAxis: item.id + "-axis",
+                    data : toTrace( values )
+                };
+
+                if( !chart ){
+                    var options = angular.copy( defaultChartOptions );
+                    options.series = [serie];
+                    options.yAxis = axis;
+
+                    chart = new Highcharts.StockChart( options );
+                    self.chart = chart;
+
+                }else{
+                    axis.opposite = true;
+                    chart.addAxis( axis );
+                    chart.addSeries( serie );
+                }
+
+            }
+
+            function removeSerie( item ){
+                // remove from graph
+                chart.get( item.id + "-serie" ).remove();
+                chart.get( item.id + "-axis" ).remove();
+            }
+
+
+            function toTrace( measures ){
+
+                var results = [];
+                angular.forEach( measures, function( m ){
+                    results.push( [new Date( m.timestamp ).getTime(), m.value] );
+                } );
+
+                return results;
+            }
+
+            //##------------utils
+
+            function _handleError( error ){
+                console.log( error );
+            }
+
+            function applyDate( d ){
+                self.date = angular.copy( d );
+                var selected = $filter( 'selected' )( self.captorsHierarchy );
+                chart = null;
+                angular.forEach( selected, getValues );
+            }
+
+            function applyAxisChanges( reset ){
+                var elem = self.modifiedAxis.elem;
+                if( reset ){
+                    self.modifiedAxis.min = elem.mydefaults[0];
+                    self.modifiedAxis.max = elem.mydefaults[1];
+
+                }
+                elem.setExtremes( self.modifiedAxis.min, self.modifiedAxis.max );
+            }
+
+
+            function getValues( item ){
+                RestService.getValues( {
+                    cid : item.id,
+                    from: moment( self.date.from ).format( "YYYY-MM-DDTHH:mm:ssZ" ),
+                    to  : moment( self.date.to ).format( "YYYY-MM-DDTHH:mm:ssZ" )
+                }, function( results ){
+                    addSerie( item, results.values );
+                }, _handleError );
             }
 
         }
-
-        function removeSerie( item ){
-            // remove from graph
-            chart.get( item.id + "-serie" ).remove();
-            chart.get( item.id + "-axis" ).remove();
-        }
-
-
-        function toTrace( measures ){
-
-            var results = [];
-            angular.forEach( measures, function( m ){
-                results.push( [new Date( m.timestamp ).getTime(), m.value] );
-            } );
-
-            return results;
-        }
-
-        //##------------utils
-
-        function _handleError( error ){
-            console.log( error );
-        }
-
-        function applyDate( d ){
-            self.date = angular.copy( d );
-            var selected = $filter( 'selected' )( self.captorsHierarchy );
-            chart = null;
-            angular.forEach( selected, getValues );
-        }
-
-
-        function getValues( item ){
-            RestService.getValues( {
-                cid : item.id,
-                from: moment( self.date.from ).format( "YYYY-MM-DDTHH:mm:ssZ" ),
-                to  : moment( self.date.to ).format( "YYYY-MM-DDTHH:mm:ssZ" )
-            }, function( results ){
-                addSerie( item, results.values );
-            }, _handleError );
-        }
-
     }
-}());
+    ()
+)
+;
