753fcce50ef9443841ab179bae96456899e3770a
Lucy Linder <lucy.derlin@gmail.com>
2017-02-21 09:33:56 +0100

    use file session store and add maxAge to cookies

--- server.js
+++ server.js
@@ -4,127 +4,148 @@
 var API_SERVER_HOST = "http://10.10.10.102:8456/api";
 
 // call the packages we need
-var express = require( 'express' );
-var proxy = require( 'http-proxy' ).createProxyServer( {host: API_SERVER_HOST} );
-var session = require( 'express-session' );
-var request = require( 'request' );
+var express = require('express');
+var proxy = require('http-proxy').createProxyServer({host: API_SERVER_HOST});
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+var request = require('request');
 
 var port = process.env.PORT || 8088;  // set our port
 var app = express();                  // define our app using express
 
 // ======================== setup resources
 
-app.use( session( {secret: 'bbdata2-l34DDlk3Qo2'} ) );      // use express sessions
+app.use(session({                        // use express sessions
+    name: 'bbwebapp-session-cookie',
+    secret: 'lkjDDALJE&&%ç"',
+    cookie: {maxAge: 60 * 1000},
+    saveUninitialized: true,
+    resave: true,
+    store: new FileStore()
+}));
 
-app.use( express.static( 'webapp/common' ) );               // vendors and assets directory
-app.use( '/auth', express.static( 'webapp/login' ) );      // login resources
-app.all( '/secured/*', function( req, res, next ){          // secured resource reachable only when logged in
-    if( isLoggedIn( req.session ) ){
+app.use(express.static('webapp/common'));               // vendors and assets directory
+app.use('/auth', express.static('webapp/login'));      // login resources
+app.all('/secured/*', function (req, res, next) {          // secured resource reachable only when logged in
+    if (isLoggedIn(req.session)) {
         next();
-    }else{
-        res.redirect( '/auth' );
+    } else {
+        res.redirect('/auth');
     }
-} );
-app.use( '/secured', express.static( 'webapp/secured' ) );  // secured resources (auth required)
+});
+app.use('/secured', express.static('webapp/secured'));  // secured resources (auth required)
 
 
 // ======================== redirects
 
-app.get( '/', function( req, res, next ){                   // redirect when asking for root
-    console.log( "/ asked. Redirect." );
-    if( isLoggedIn( req.session ) ){
-        res.redirect( '/secured' );
-    }else{
+app.get('/', function (req, res, next) {                   // redirect when asking for root
+    console.log("/ asked. Redirect.");
+    if (isLoggedIn(req.session)) {
+        res.redirect('/secured');
+    } else {
         // TODO for DEBUG only
-        // var sess = req.session;
+        var sess = req.session;
         // sess.bbuser = 1;
         // sess.bbtoken = "lala";
         // sess.loggedIn = true;
         // res.redirect( '/secured' );
-         res.redirect( '/auth' );
+        res.redirect('/auth');
     }
-} );
+});
 
 // TODO: find a way to redirect after an http-proxy pipe instead of creating a whole new request
-app.get( '/logout', function( req, res, next ){             // logout utility
+app.get('/logout', function (req, res, next) {             // logout utility
     var sess = req.session;
 
-    request( {
-        url    : API_SERVER_HOST + '/logout',
+    request({
+        url: API_SERVER_HOST + '/logout',
         method: "POST",
         headers: {
             "content-type": "application/json",
-            "bbuser" : sess.bbuser,
+            "bbuser": sess.bbuser,
             "bbtoken": sess.bbtoken
         }
-    }, function( error, response, body ){
-        console.log( "logout done", body, error );
-    } );
-    clearSession( sess );
-    res.redirect( "/" );
-} );
+    }, function (error, response, body) {
+        console.log("logout done", body, error);
+    });
+    clearSession(sess);
+    res.redirect("/");
+});
 
-app.post('/logid', function(req, res, next){
+app.post('/logid', function (req, res, next) {
     // allow angular to know which apikey is currently used
     res.send({id: req.session.apikeyId});
 });
 // ======================== proxy management
 
 
-app.use( '/api', function( req, res, next ){                // setup redirect to api calls
-    proxy.web( req, res, {
+app.use('/api', function (req, res, next) {                // setup redirect to api calls
+    proxy.web(req, res, {
         target: API_SERVER_HOST
-    }, next );
-} );
+    }, next);
+
+});
 
 
-proxy.on( 'proxyReq', function( proxyReq, req, res ){       // add authentication headers for api calls
-    setBbBeaders( proxyReq, req.session );
-} );
+proxy.on('proxyReq', function (proxyReq, req, res) {       // add authentication headers for api calls
+    setBbBeaders(proxyReq, req.session);
+});
 
 
-proxy.on( 'proxyRes', function( proxyRes, req, res ){       // update session after api calls on login/logout
+proxy.on('proxyRes', function (proxyRes, req, res) {       // update session after api calls on login/logout
     // handle login and logout response
-    if( req.url == '/login' ){
+    if (req.url == '/login') {
         // get response body
-        proxyRes.on( 'data', function( dataBuffer ){
+        proxyRes.on('data', function (dataBuffer) {
             // decode body
-            var data = dataBuffer.toString( 'utf8' );
-            if( data ){
-                var body = JSON.parse( data );
-                console.log( body );
-                setupSession( req.session, body );
+            var data = dataBuffer.toString('utf8');
+            if (data) {
+                var body = JSON.parse(data);
+                console.log(body);
+                setupSession(req.session, body);
             }
-        } );
+        });
 
-    }else if( req.url == '/logout' ){
-        clearSession( req.session );
+    } else if (req.url == '/logout') {
+        clearSession(req.session);
     }
 
     // logging
-    console.log( "request : url=", req.url, req.body ? "body=" + req.body : "" );
-    console.log( "response: status=", res.statusCode );
-} );
+    console.log("request : url=", req.url, req.body ? "body=" + req.body : "");
+    console.log("response: status=", res.statusCode);
+});
+
+// Listen for the `error` event on `proxy`.
+proxy.on('error', function (err, req, res) {
+    console.log("ERROR", res);
+    console.log(res.body);
+    res.writeHead(500, {
+        'Content-Type': 'text/plain'
+    });
+
+    res.end('Something went wrong. And we are reporting a custom error message.');
+});
 
 
 // ======================== launch the server
 
-app.listen( port, function(){
-    console.log( 'Magic happens on port ' + port );
-} );
+app.listen(port, function () {
+    console.log('Magic happens on port ' + port);
+});
 
 
 // ========================  session management utilities
 
-function setBbBeaders( req, sess ){
-    if( isLoggedIn( sess ) ){
-        req.setHeader( 'bbuser', sess.bbuser );
-        req.setHeader( 'bbtoken', sess.bbtoken );
+function setBbBeaders(req, sess) {
+    console.log("bb set header: ", sess);
+    if (isLoggedIn(sess)) {
+        req.setHeader('bbuser', sess.bbuser);
+        req.setHeader('bbtoken', sess.bbtoken);
     }
 }
 
-function setupSession( sess, jsonResponse ){
-    if( jsonResponse.secret ){
+function setupSession(sess, jsonResponse) {
+    if (jsonResponse.secret) {
         // get got a new apikey, save it to the session
         sess.bbuser = jsonResponse.userId;
         sess.bbtoken = jsonResponse.secret;
@@ -133,10 +154,11 @@ function setupSession( sess, jsonResponse ){
     }
 }
 
-function clearSession( sess ){
-    sess.loggedIn = false;
+function clearSession(sess) {
+    sess.destroy();
+
 }
 
-function isLoggedIn( sess ){
+function isLoggedIn(sess) {
     return sess.loggedIn;
 }
